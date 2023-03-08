1bda5b622c7510ca267aac8bf7b7f5e0eee1afeb
Patrik Tisoň <patriktison@gmail.com>
2023-03-08 13:44:44 +0100

    first shot

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,20 @@
 import json
-
-# import requests
-
+import boto3
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    sqs_message = json.loads(event['Records'][0]['body'])
+    commodity = sqs_message['instrument']
+    
+    # specify the DynamoDB table to retrieve data from
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('my-dynamodb-table')
+    
+    response = table.get_item(Key={'id': 'instrument'})
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    data = response['Item']['corn_data']
+    # publish the data to an SNS topic
+    sns = boto3.client('sns')
+    sns.publish(
+        TopicArn='arn:aws:sns:us-west-2:123456789012:my-sns-topic',
+        Message=json.dumps({'instrument': commodity, 'data': data})
+    )
